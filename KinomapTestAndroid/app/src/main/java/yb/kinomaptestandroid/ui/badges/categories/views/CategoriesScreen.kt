ab5186c99bcecf61f268a62f38f6f9554730b642
yoann bourgault <bourgaulty@gmail.com>
2025-08-29 12:34:45 +0200

    Categories screen title

--- KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/badges/categories/views/CategoriesScreen.kt
+++ KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/badges/categories/views/CategoriesScreen.kt
@@ -3,13 +3,18 @@ package yb.kinomaptestandroid.ui.badges.categories.views
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import yb.kinomaptestandroid.R
 import yb.kinomaptestandroid.ui.navigation.models.BadgeCategory
 
 @Composable
@@ -34,6 +39,14 @@ fun CategoriesScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top
             ) {
+                item {
+                    Text(
+                        modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp),
+                        text = stringResource(R.string.category_screen_title),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
                 categories.forEach { category ->
                     item {
                         CategoryItemView(
