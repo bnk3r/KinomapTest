f1251ab8745791ebca29e2dfe9348300cba9ca08
yoann bourgault <bourgaulty@gmail.com>
2025-08-29 11:58:32 +0200

    Animated navigation for Badge Details Screen

--- KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/navigation/views/AppNavigation.kt
+++ KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/navigation/views/AppNavigation.kt
@@ -1,5 +1,12 @@
 package yb.kinomaptestandroid.ui.navigation.views
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -42,7 +49,28 @@ fun AppNavigation(
             )
         }
 
-        composable<BadgesDetailsScreenNav> {
+        composable<BadgesDetailsScreenNav>(
+            enterTransition = {
+                fadeIn(
+                    animationSpec = tween(
+                        300, easing = LinearEasing
+                    )
+                ) + slideIntoContainer(
+                    animationSpec = tween(300, easing = EaseIn),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            },
+            exitTransition = {
+                fadeOut(
+                    animationSpec = tween(
+                        300, easing = LinearEasing
+                    )
+                ) + slideOutOfContainer(
+                    animationSpec = tween(300, easing = EaseOut),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }
+        ) {
             it.arguments?.let { args ->
                 val badgeId = args.getInt("badgeId", -1)
                 if (badgeId == -1) return@let
