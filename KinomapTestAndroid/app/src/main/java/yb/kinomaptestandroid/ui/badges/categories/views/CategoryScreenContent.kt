7b985c4c0a84d210cef678cbe1a7317e39a8aba8
yoann bourgault <bourgaulty@gmail.com>
2025-09-04 17:00:51 +0200

    FAB to scroll to start of the categories
    - appears only when 1st badge row is not visible

--- KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/badges/categories/views/CategoryScreenContent.kt
+++ KinomapTestAndroid/app/src/main/java/yb/kinomaptestandroid/ui/badges/categories/views/CategoryScreenContent.kt
@@ -1,17 +1,29 @@
 package yb.kinomaptestandroid.ui.badges.categories.views
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import yb.kinomaptestandroid.ui.navigation.models.BadgeCategory
 
 @Composable
@@ -20,6 +32,9 @@ fun CategoryScreenContent(
     categories: List<BadgeCategory>,
     onClickBadgeView: (id: Int) -> Unit
 ) {
+    val listState = rememberLazyGridState()
+    val coroutineScope = rememberCoroutineScope()
+
     val configuration = LocalConfiguration.current
     val orientation = configuration.orientation
 
@@ -28,43 +43,80 @@ fun CategoryScreenContent(
         else -> 5
     }
 
-    LazyVerticalGrid(
-        modifier = modifier.padding(horizontal = 16.dp),
-        columns = GridCells.Fixed(gridColumns)
-    ) {
-        categories.forEachIndexed { i, category ->
-            if (i != 0) {
-                item(
-                    span = { GridItemSpan(gridColumns) }
-                ) {
-                    Spacer(
-                        modifier = Modifier.height(16.dp)
-                    )
-                }
+    val isFirstBadgeVisible =
+        remember {
+            derivedStateOf {
+                if (listState.layoutInfo.visibleItemsInfo.isEmpty()) true
+                else listState.layoutInfo.visibleItemsInfo[0].index <= gridColumns + 1
             }
-            item(
-                span = { GridItemSpan(gridColumns) }
+        }
+
+    if (categories.isNotEmpty()) {
+        Box(
+            modifier = modifier
+        ) {
+            LazyVerticalGrid(
+                modifier = modifier.padding(horizontal = 16.dp),
+                columns = GridCells.Fixed(gridColumns),
+                state = listState
             ) {
-                CategoryItemView(
-                    modifier = Modifier.fillMaxWidth(), category = category
-                )
-            }
-            if (category.badges.isNotEmpty()) {
-                item(
-                    span = { GridItemSpan(gridColumns) }
-                ) {
-                    Spacer(
-                        modifier = Modifier.height(8.dp)
-                    )
+                categories.forEachIndexed { i, category ->
+                    if (i != 0) {
+                        item(
+                            span = { GridItemSpan(gridColumns) }
+                        ) {
+                            Spacer(
+                                modifier = Modifier.height(16.dp)
+                            )
+                        }
+                    }
+                    item(
+                        span = { GridItemSpan(gridColumns) }
+                    ) {
+                        CategoryItemView(
+                            modifier = Modifier.fillMaxWidth(), category = category
+                        )
+                    }
+                    if (category.badges.isNotEmpty()) {
+                        item(
+                            span = { GridItemSpan(gridColumns) }
+                        ) {
+                            Spacer(
+                                modifier = Modifier.height(8.dp)
+                            )
+                        }
+                    }
+                    category.badges.forEach { badge ->
+                        item {
+                            BadgeItemView(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                badge = badge,
+                                onClick = { onClickBadgeView(badge.id) })
+                        }
+                    }
                 }
             }
-            category.badges.forEach { badge ->
-                item {
-                    BadgeItemView(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        badge = badge,
-                        onClick = { onClickBadgeView(badge.id) })
+
+            if (!isFirstBadgeVisible.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.BottomEnd
+                ) {
+                    FloatingActionButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                listState.animateScrollToItem(index = 0)
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowUp,
+                            contentDescription = null
+                        )
+                    }
                 }
             }
         }
